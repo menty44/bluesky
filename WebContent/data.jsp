d4269c44821a98e246e2ae398bafb4803ce48cfd
fred <fred.oluoch@impalapay.com>
2017-03-15 13:49:51 +0300

    wen v3

--- WebContent/data.jsp
+++ WebContent/data.jsp
@@ -4,9 +4,44 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
 <title>Insert title here</title>
 </head>
 <body>
+<style type="text/css" title="currentStyle">
+@import "../resources/css/demo_table.css";
+</style>
 
+<table cellpadding="0" cellspacing="0" border="0"
+id="tableId">
+<thead>
+<tr>
+<th width="10%">First Name</th>
+<th width="10%">Last Name</th>
+<th width="10%">Address 1</th>
+<th width="10%">Address 2</th>
+</tr>
+</thead>
+</table>
+
+<script type="text/javascript" charset="utf-8">
+$(document).ready(function() {
+var oTable = $('#tableId').dataTable( {
+"processing": true,
+"ajax": {
+"url": "/FeedSummaryUpdate/FeedServlet",
+"dataSrc": "demo",
+"type": "GET"
+}
+} );
+} );
+</script>
+
+
+
+<script type="text/javascript"
+charset="utf-8" src="/DataTables/js/jquery.js"></script>
+<script type="text/javascript"
+charset="utf-8" src="/DataTables/js/jquery.dataTables.js"></script>
 </body>
 </html>
---
+++ src/com/javawebtutor/controller/FeedServlet.java
@@ -0,0 +1,45 @@
+package com.javawebtutor.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FeedServlet
+ */
+@WebServlet("/FeedServlet")
+public class FeedServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FeedServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+			PrintWriter out = response.getWriter();
+			String json = "{\"demo\": [[\"First Name\",\"Last Name\",\"Address1\",\"Address2\"],[\"First Name\",\"Last Name\",\"Address1\",\"Address2\"]]}";
+					out.println(json);
+			}
+
+}
